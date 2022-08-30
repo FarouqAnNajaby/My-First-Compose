aa74a3e624929c3681480c6945bcb8f02378413c
PSEDP3010\eats <farouq@mediainovasi.id>
2022-08-30 11:33:39 +0700

    create layout

---
+++ app/src/main/java/com/example/myfirstcompose/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.myfirstcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myfirstcompose.ui.theme.MyFirstComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyFirstComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    createCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun createCard(){
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier.width(200.dp)
+            .height(390.dp)
+            .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            backgroundColor = Color.White,
+            elevation = 4.dp) {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    createCard()
+}
